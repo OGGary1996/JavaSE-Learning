de194e3e12db851af68bab52b719da5c447176d4
ke_zhang <zhangke918525@gmail.com>
2024-09-15 18:58:22 -0600

    add some training package

---
+++ src/training/condition_statements.java
@@ -0,0 +1,17 @@
+package training;
+
+public class condition_statements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// if-else
+		int a = 28;
+		if (a > 10 && a <= 20) {
+			System.out.println("hello");
+		}
+		else {
+			System.out.println("bye");
+		}
+	}
+
+}
