107ff2fa75ec534abee67e64b2d39775557f0a2d
ke_zhang <zhangke918525@gmail.com>
2024-09-15 21:07:29 -0600

    add some if-else if

--- src/training/condition_statements.java
+++ src/training/condition_statements.java
@@ -12,6 +12,19 @@ public class condition_statements {
 		else {
 			System.out.println("bye");
 		}
+		
+		// if-else if
+		// example 比较三个数字大小
+		int x = 100 , y = 400 , z = 200;
+		if (x > y && x >z) {
+			System.out.println(x +" is the Greatest number");
+		}
+		else if (y > z) {
+			System.out.println(y +" is the Greatest number");
+		}
+		else {
+			System.out.print(z +" is the Greatest number");
+		}
 	}
 
 }
