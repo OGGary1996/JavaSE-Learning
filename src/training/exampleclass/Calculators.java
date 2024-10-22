788c0d21d1a2bf65c7334ecf1b3acb0c4b21f5e8
OGGary1996 <zhangke918525@gmail.com>
2024-10-21 20:08:17 -0600

    rezerblade14

--- src/training/exampleclass/Calculators.java
+++ src/training/exampleclass/Calculators.java
@@ -1,26 +1,26 @@
 package training.exampleclass;
 
 public class Calculators {
-	@Override
-	public String toString(){ // 重写Object class中的默认的toString()方法
-		return "This is a calculator class made by KE";
-	}
+    @Override
+    public String toString(){ // 重写Object class中的默认的toString()方法
+        return "This is a calculator class made by KE";
+    }
 
-	public int add(int num1 , int num2) {
-		int result = num1 + num2;
-		return result;
-	}
-	// method overloading 
-	public int add(int num1, int num2, int num3) {
-		int result = num1 + num2 + num3;
-		return result;
-	}
-	public double add(double num1 , int num2) {
-		double result = num1 + num2;
-		return result;
-	}
+    public int add(int num1 , int num2) {
+        int result = num1 + num2;
+        return result;
+    }
+    // method overloading 
+    public int add(int num1, int num2, int num3) {
+        int result = num1 + num2 + num3;
+        return result;
+    }
+    public double add(double num1 , int num2) {
+        double result = num1 + num2;
+        return result;
+    }
 
-	public int sub(int num1,int num2){
+    public int sub(int num1,int num2){
         return (num1 - num2);
     }
 
@@ -30,6 +30,5 @@ public class Calculators {
 
     public double div(double num1,double num2){
         return (num1 / num2);
+    }
 }
-}
-
