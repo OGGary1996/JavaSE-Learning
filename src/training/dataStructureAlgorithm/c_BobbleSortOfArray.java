52991be00601caca483e8ceda5f9cf5e4adb6a5b
OGGary1996 <zhangke918525@gmail.com>
2024-12-19 14:35:42 -0600

    change the range of array

--- src/training/dataStructureAlgorithm/c_BobbleSortOfArray.java
+++ src/training/dataStructureAlgorithm/c_BobbleSortOfArray.java
@@ -5,7 +5,7 @@ import training.dataStructureAlgorithm.algorithmOfArray.SortOfArray;
 
 public class c_BobbleSortOfArray {
     public static void main(String[] args) {
-        int[] nums = new int[100];
+        int[] nums = new int[10];
         for (int i = 0; i < nums.length; i++){
             nums[i] = (int)(Math.random()*100); // Randomly generate elements and insert them into the array
         }
--- src/training/dataStructureAlgorithm/e_InsertionSortOfArray.java
+++ src/training/dataStructureAlgorithm/e_InsertionSortOfArray.java
@@ -4,7 +4,7 @@ import training.dataStructureAlgorithm.algorithmOfArray.SortOfArray;
 
 public class e_InsertionSortOfArray {
     public static void main(String[] args){
-        int[] nums = new int[100];
+        int[] nums = new int[10];
         for (int i = 0; i < nums.length; i++){
             nums[i] = (int)(Math.random()*100);
         }
